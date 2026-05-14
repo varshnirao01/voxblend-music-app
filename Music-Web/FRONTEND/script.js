4bc2fc14132b45405ae6329b13423075a4e203ba
varshnirao01 <malrajvarshnil@gmail.com>
2026-05-15 00:34:27 +0530

    fixed duplicate API_URL error

--- Music-Web/FRONTEND/script.js
+++ Music-Web/FRONTEND/script.js
@@ -168,7 +168,6 @@
 //   updateSubscription();
 // });
 const API_URL = "https://voxblend-music-app-1.onrender.com";
-const API_URL = "https://voxblend-music-app-1.onrender.com";
 
 const audio       = document.getElementById('audioPlayer');
 const vinyl       = document.getElementById('vinyl');
